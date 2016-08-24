92ba3052904e5855c9df550282651d6fb7e0b88c
Franco Risso <franco@720desarrollos.com>
2016-08-24 15:52:30 -0300

    set up bot message platform

--- bot/app.js
+++ bot/app.js
@@ -84,7 +84,7 @@ app.get('/webhook', function(req, res) {
  */
 app.post('/webhook', function (req, res) {
   var data = req.body;
-
+  console.info('received webhook', data);
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
