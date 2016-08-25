651c43277185b9476d26e474c264cc47a8b45975
Franco Risso <franco@720desarrollos.com>
2016-08-25 13:30:38 -0300

    set up bot message platform

--- bot/src/response.js
+++ bot/src/response.js
@@ -6,16 +6,34 @@ Talk.prototype = {
   get greetings() {
     return 'Hola! En que puedo ayudarte?';
   },
+  get reservationText() {
+    return 'Podemos realizar una reserva. Para que fecha querés el turno?';
+  },
+  get dirty() {
+    const msgs = ['Apa... bueno, no se que hacer', '😘', '🙈 🙉 🙊', 'Me confunde señor!'];
+    return msgs[Math.floor(Math.rand()*msgs.length-1)];
+  },
+  searchInMessage: function(words) {
+    return this.message.split(/\s+/).reduce(function(acc, text){
+      return acc || words.indexOf(text.replace(/(\W)/gi,'').toLowerCase())!==-1;
+    }, false);
+  },
+  hasDirty: function() {
+    return this.searchInMessage(['sexo','mierda','puto','pete']);
+  },
   hasGreetings: function() {
-    const greetings = [
+    return this.searchInMessage([
       'hola',
       'buenas',
       'hello',
       'hi',
-    ];
-    return this.message.split(/\s+/).reduce(function(acc, text){
-      return acc || greetings.indexOf(text.replace(/(\W)/gi,'').toLowerCase())!==-1;
-    }, false);
+    ]);
+  },
+  hasReservationRequest: function(){
+    return this.searchInMessage([
+      'turno',
+      'reserva',
+    ]);
   },
   toString: function() {
     return this.message;
@@ -23,11 +41,21 @@ Talk.prototype = {
 };
 
 const getResponse = function(message) {
+  const responses = [];
   const talk = new Talk(message);
   if (talk.hasGreetings()) {
-    return talk.greetings;
+    responses.push(talk.greetings);
   }
-  return 'Perdón, no entiendo a que te refieres :(';
+  if (talk.hasReservationRequest()) {
+    responses.push(talk.reservationText);
+  }
+  if (talk.hasDirty()) {
+    responses.push(talk.dirty);
+  }
+  if (!responses.length) {
+    responses.push('Perdón, no entiendo a que te refieres :(');
+  }
+  return responses;
 }
 
 module.exports.getResponse = getResponse;
